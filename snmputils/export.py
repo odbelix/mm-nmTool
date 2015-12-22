ac1ca96eb1d29398c37c73179d3144bbdcecfe35
Manuel Alejandro Moscoso Dominguez <manuel.moscoso.d@gmail.com>
2015-12-22 10:02:26 -0300

    Files updated

--- snmputils/export.py
+++ snmputils/export.py
@@ -18,33 +18,3 @@
 # Manuel Moscoso Dominguez <manuel.moscoso.d@gmail.com>
 ########################################################################
 
-## Change format of Mac, From SNMP format to HexaFormat
-def changeMacFormat(macaddress):
-    result = macaddress.replace(" ",":")
-    result = result.replace("\"","")
-    result = result.rstrip(":")
-    result = result.lstrip(":")
-    result = result.lower()
-    return result
-
-def HextoStringIp(hexString):
-    result = hexString.split("Hex-STRING:")[1]
-    result = result[1:-1].split(" ")
-    stringIp = ""
-    for strData in result:
-         stringIp += str(int(strData, 16)) + "."
-    return stringIp[:-1]
-
-def OutputToString(outputName):
-    result = outputName.split("STRING:")#[1]
-    if len(result) == 2:
-    	result = result[1][1:-1]
-    	return result.replace("\"","").lstrip()
-    else:
-    	print outputName
-    	return None
-
-def OutputToStringFromInteger(outputName):
-    result = outputName.split("INTEGER:")
-    result = result[1].replace(" ","")
-    return result
